54d184ba234212157e0a65f73ebd8e0a06c25259
Vertmo <basile.pesin@gmail.com>
2018-10-21 21:41:25 +0200

    Added parsing of command line args

--- app/processingml.ml
+++ app/processingml.ml
@@ -1,4 +1,4 @@
-(******************************************************************************)
+(*******************************************r**********************************)
 (*                                                                            *)
 (*                                  ProcessingML                              *)
 (*                                  Basile Pesin                              *)
@@ -6,7 +6,19 @@
 (*                                                                            *)
 (******************************************************************************)
 
-open LibPML
-
 (** Main module, containing interpretation for command line arguments *)
-let _ = Test.hello ()
+
+let usage = "usage: " ^ Sys.argv.(0) ^ " [--run] [--build] <input_file>"
+
+type steps = Run | Build
+
+let step = ref Run
+
+let speclist = [
+  ("--run", Arg.Unit (fun () -> step := Run), ": Compile and run a sketch (default)");
+  ("--build", Arg.Unit (fun () -> step := Run), ": Compile a sketch")
+]
+
+let main filename _ = print_endline filename (* TODO *)
+
+let _ = Arg.parse speclist (fun x -> main x step) usage
