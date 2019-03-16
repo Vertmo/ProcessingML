c7ed02b400994f98dcaeafab94fd8f10933b4746
Basile Pesin <basile.pesin@gmail.com>
2019-03-16 18:40:38 +0100

    A bit of cleanup

--- samples/first/first.ml
+++
@@ -1,7 +0,0 @@
-(** A first sketch, serving only to demonstrate module loading by the app *)
-
-open LibPML
-
-let setup () = print_endline "setup"; 0
-
-let draw i = print_int i; Test.hello (); (i+1)
--- tests/tests.ml
+++
@@ -1,6 +0,0 @@
-open OUnit2
-
-let suite =
-  "Suite" >::: []
-
-let _ = run_test_tt_main suite
